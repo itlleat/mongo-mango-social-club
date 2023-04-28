51a06a7f47644acb53d3c39c8d53089d6cbf3593
itlleat <scott.schulman84@gmail.com>
2023-04-28 16:27:06 -0600

    populated models, need to test

--- models/Thought.js
+++ models/Thought.js
@@ -1,49 +1,146 @@
 // Dependencies
+const { Schema, model } = require('mongoose');
+const validate = require('mongoose-validator');
 
 // Thought:
+const thoughtSchema = new Schema(
+    {
+        // thoughtText
+        // String
+        // Required
+        // Must be between 1 and 280 characters
 
-// thoughtText
 
-// String
-// Required
-// Must be between 1 and 280 characters
-// createdAt
+        thoughtText: {
+            type: String,
+            required: true,
+            var lengthValidator = [
+                Validate({
+                    validator: 'isLength',
+                    arguments: [1, 280],
+                    message: 'Thoughts must be between 1 and 280 characters!'
+                })
+            ],
+        },
 
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-// username (The user that created this thought)
+        // createdAt
+        // Date
+        // Set default value to the current timestamp
+        // Use a getter method to format the timestamp on query
 
-// String
-// Required
-// reactions (These are like replies)
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function (timestamp) {
+                return timestamp.toLocaleString();
+            }
+        },
 
-// Array of nested documents created with the reactionSchema
+
+        // username (The user that created this thought)
+
+        // String
+        // Required
+
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+
+        // reactions (These are like replies)
+        // Array of nested documents created with the reactionSchema
+
+        reactions: [reactionSchema]
+
+    },
+    // ***LOOK AT THIS***
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
 
 // Schema Settings:
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+// ** Need to finish! **
 
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
 // Reaction (SCHEMA ONLY)
 
-// reactionId
+const reactionSchema = new Schema(
+    {
 
-// Use Mongoose's ObjectId data type
-// Default value is set to a new ObjectId
-// reactionBody
+        // reactionId*
 
-// String
-// Required
-// 280 character maximum
-// username
+        // Use Mongoose's ObjectId data type
+        // Default value is set to a new ObjectId
 
-// String
-// Required
-// createdAt
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+            required: true
+        },
 
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-// Schema Settings:
+
+        // reactionBody*
+
+        // String
+        // Required
+        // 280 character maximum
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+
+
+        // username*
+
+        // String
+        // Required
+
+
+        username: {
+            type: String,
+            required: true
+        },
+
+
+        // createdAt*
+
+        // Date
+        // Set default value to the current timestamp
+        // Use a getter method to format the timestamp on query
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => {
+                return new Date(timestamp).toLocaleDateString('en-US', {
+                    weekday: 'short',
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: true
+                });
+            }
+        }
+
+    });
+
+
 
 // This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+
+module.exports = reactionSchema;
